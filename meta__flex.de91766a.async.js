b62315ba65197c9f90b77d020e18a09fd9a797dd
ZhaoFxxkSky <36238435+ZhaoFxxkSky@users.noreply.github.com>
2025-09-11 07:24:11 +0000

    Deploying to gh-pages from @ ZhaoFxxkSky/dt-design@298b0f627422f418285e5e35da80334db220d350 🚀

---
+++ meta__flex.de91766a.async.js
@@ -0,0 +1,168 @@
+"use strict";(self.webpackChunkdt_design=self.webpackChunkdt_design||[]).push([[8674],{35392:function(o,t,n){n.r(t),n.d(t,{demos:function(){return d}});var e=n(75271),d={"flex-demo-basic":{component:e.memo(e.lazy(function(){return Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(2208),n.e(2276),n.e(4072),n.e(3880),n.e(4886),n.e(7145),n.e(6117),n.e(1699),n.e(6455),n.e(4914),n.e(3304)]).then(n.bind(n,28281))})),asset:{type:"BLOCK",id:"flex-demo-basic",refAtomIds:["flex"],dependencies:{"index.tsx":{type:"FILE",value:n(19800).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-design":{type:"NPM",value:"1.0.0"}},entry:"index.tsx",title:"\u57FA\u672C\u5E03\u5C40"},context:void 0,renderOpts:void 0},"flex-demo-align":{component:e.memo(e.lazy(function(){return Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(2208),n.e(2276),n.e(4072),n.e(3880),n.e(4886),n.e(7145),n.e(6117),n.e(1699),n.e(6455),n.e(4914),n.e(3304)]).then(n.bind(n,26587))})),asset:{type:"BLOCK",id:"flex-demo-align",refAtomIds:["flex"],dependencies:{"index.tsx":{type:"FILE",value:n(81478).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-design":{type:"NPM",value:"1.0.0"}},entry:"index.tsx",title:"\u5BF9\u9F50\u65B9\u5F0F"},context:void 0,renderOpts:void 0},"flex-demo-gap":{component:e.memo(e.lazy(function(){return Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(2208),n.e(2276),n.e(4072),n.e(3880),n.e(4886),n.e(7145),n.e(6117),n.e(1699),n.e(6455),n.e(4914),n.e(3304)]).then(n.bind(n,8874))})),asset:{type:"BLOCK",id:"flex-demo-gap",refAtomIds:["flex"],dependencies:{"index.tsx":{type:"FILE",value:n(93267).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-design":{type:"NPM",value:"1.0.0"}},entry:"index.tsx",title:"\u8BBE\u7F6E\u95F4\u9699"},context:void 0,renderOpts:void 0},"flex-demo-wrap":{component:e.memo(e.lazy(function(){return Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(2208),n.e(2276),n.e(4072),n.e(3880),n.e(4886),n.e(7145),n.e(6117),n.e(1699),n.e(6455),n.e(4914),n.e(3304)]).then(n.bind(n,67938))})),asset:{type:"BLOCK",id:"flex-demo-wrap",refAtomIds:["flex"],dependencies:{"index.tsx":{type:"FILE",value:n(32756).Z},react:{type:"NPM",value:"18.3.1"},"dt-design":{type:"NPM",value:"1.0.0"}},entry:"index.tsx",title:"\u81EA\u52A8\u6362\u884C"},context:void 0,renderOpts:void 0},"flex-demo-combination":{component:e.memo(e.lazy(function(){return Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(2208),n.e(2276),n.e(4072),n.e(3880),n.e(4886),n.e(7145),n.e(6117),n.e(1699),n.e(6455),n.e(4914),n.e(3304)]).then(n.bind(n,55727))})),asset:{type:"BLOCK",id:"flex-demo-combination",refAtomIds:["flex"],dependencies:{"index.tsx":{type:"FILE",value:n(87848).Z},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-design":{type:"NPM",value:"1.0.0"}},entry:"index.tsx",title:"\u7EC4\u5408\u4F7F\u7528"},context:void 0,renderOpts:void 0}}},1318:function(o,t,n){n.r(t),n.d(t,{texts:function(){return e}});const e=[{value:"\u9700\u8981 Flex \u5E03\u5C40",paraId:0,tocIndex:1},{value:"\u53C2\u6570",paraId:1,tocIndex:8},{value:"\u8BF4\u660E",paraId:1,tocIndex:8},{value:"\u7C7B\u578B",paraId:1,tocIndex:8},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:8},{value:"vertical",paraId:1,tocIndex:8},{value:"flex \u4E3B\u8F74\u7684\u65B9\u5411\u662F\u5426\u5782\u76F4",paraId:1,tocIndex:8},{value:"boolean",paraId:1,tocIndex:8},{value:"false",paraId:1,tocIndex:8},{value:"wrap",paraId:1,tocIndex:8},{value:"\u4E3B\u8F74\u6362\u884C",paraId:1,tocIndex:8},{value:"flex-wrap",paraId:1,tocIndex:8},{value:"nowrap",paraId:1,tocIndex:8},{value:"justify",paraId:1,tocIndex:8},{value:"justify-content",paraId:1,tocIndex:8},{value:"justify-content",paraId:1,tocIndex:8},{value:"normal",paraId:1,tocIndex:8},{value:"align",paraId:1,tocIndex:8},{value:"align-items",paraId:1,tocIndex:8},{value:"align-items",paraId:1,tocIndex:8},{value:"normal",paraId:1,tocIndex:8},{value:"flex",paraId:1,tocIndex:8},{value:"flex",paraId:1,tocIndex:8},{value:"flex",paraId:1,tocIndex:8},{value:"normal",paraId:1,tocIndex:8},{value:"gap",paraId:1,tocIndex:8},{value:"gap",paraId:1,tocIndex:8},{value:"gap",paraId:1,tocIndex:8},{value:"0",paraId:1,tocIndex:8},{value:"children",paraId:1,tocIndex:8},{value:"\u5C55\u793A\u5185\u5BB9",paraId:1,tocIndex:8},{value:"React.ReactNode",paraId:1,tocIndex:8},{value:"-",paraId:1,tocIndex:8}]},81478:function(o,t){t.Z=`import React from 'react';
+import { Button, Segmented } from 'antd';
+import type { FlexProps } from 'dt-design';
+import { Flex } from 'dt-design';
+
+const boxStyle: React.CSSProperties = {
+    width: '100%',
+    height: 120,
+    borderRadius: 6,
+    border: '1px solid #40a9ff',
+};
+
+const justifyOptions = [
+    'flex-start',
+    'center',
+    'flex-end',
+    'space-between',
+    'space-around',
+    'space-evenly',
+];
+
+const alignOptions = ['flex-start', 'center', 'flex-end'];
+
+const App: React.FC = () => {
+    const [justify, setJustify] = React.useState<FlexProps['justify']>(justifyOptions[0]);
+    const [alignItems, setAlignItems] = React.useState<FlexProps['align']>(alignOptions[0]);
+    return (
+        <Flex gap="middle" align="start" vertical>
+            <p>Select justify :</p>
+            <Segmented
+                options={justifyOptions}
+                onChange={(value) => setJustify(value as FlexProps['justify'])}
+            />
+            <p>Select align :</p>
+            <Segmented
+                options={alignOptions}
+                onChange={(value) => {
+                    setAlignItems(value as FlexProps['align']);
+                }}
+            />
+            <Flex style={boxStyle} justify={justify} align={alignItems}>
+                <Button type="primary">Primary</Button>
+                <Button type="primary">Primary</Button>
+                <Button type="primary">Primary</Button>
+                <Button type="primary">Primary</Button>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default App;
+`},19800:function(o,t){t.Z=`import React from 'react';
+import { Radio } from 'antd';
+import { Flex } from 'dt-design';
+
+const baseStyle: React.CSSProperties = {
+    width: '25%',
+    height: 54,
+};
+
+const App: React.FC = () => {
+    const [value, setValue] = React.useState<string>('horizontal');
+    return (
+        <Flex gap="middle" vertical>
+            <Radio.Group value={value} onChange={(e) => setValue(e.target.value)}>
+                <Radio value="horizontal">horizontal</Radio>
+                <Radio value="vertical">vertical</Radio>
+            </Radio.Group>
+            <Flex vertical={value === 'vertical'}>
+                {Array.from({ length: 4 }).map((_, i) => (
+                    <div
+                        key={i}
+                        style={{ ...baseStyle, backgroundColor: i % 2 ? '#1677ff' : '#1677ffbf' }}
+                    />
+                ))}
+            </Flex>
+        </Flex>
+    );
+};
+
+export default App;
+`},87848:function(o,t){t.Z=`import React from 'react';
+import { Card, Typography } from 'antd';
+import { Button, Flex } from 'dt-design';
+
+const cardStyle: React.CSSProperties = {
+    width: 620,
+};
+
+const imgStyle: React.CSSProperties = {
+    display: 'block',
+    width: 273,
+};
+
+const App: React.FC = () => (
+    <Card hoverable style={cardStyle} bodyStyle={{ padding: 0, overflow: 'hidden' }}>
+        <Flex justify="space-between">
+            <img
+                draggable={false}
+                alt="avatar"
+                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                style={imgStyle}
+            />
+            <Flex vertical align="flex-end" justify="space-between" style={{ padding: 32 }}>
+                <Typography.Title level={3}>
+                    \u201Cantd is an enterprise-class UI design language and React UI library.\u201D
+                </Typography.Title>
+                <Button
+                    type="primary"
+                    href="https://zhaofxxksky.github.io/dt-design/"
+                    target="_blank"
+                >
+                    Get Started
+                </Button>
+            </Flex>
+        </Flex>
+    </Card>
+);
+
+export default App;
+`},93267:function(o,t){t.Z=`import React from 'react';
+import { Radio, Slider } from 'antd';
+import type { ConfigProviderProps } from 'antd/es/config-provider';
+import { Button, Flex } from 'dt-design';
+
+type SizeType = ConfigProviderProps['componentSize'];
+
+const App: React.FC = () => {
+    const [gapSize, setGapSize] = React.useState<SizeType | 'customize'>('small');
+    const [customGapSize, setCustomGapSize] = React.useState<number>(0);
+    return (
+        <Flex gap="middle" vertical>
+            <Radio.Group value={gapSize} onChange={(e) => setGapSize(e.target.value)}>
+                {['small', 'middle', 'large', 'customize'].map((size) => (
+                    <Radio key={size} value={size}>
+                        {size}
+                    </Radio>
+                ))}
+            </Radio.Group>
+            {gapSize === 'customize' && (
+                <Slider value={customGapSize} onChange={setCustomGapSize} />
+            )}
+            <Flex gap={gapSize !== 'customize' ? gapSize : customGapSize}>
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="link">Link</Button>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default App;
+`},32756:function(o,t){t.Z=`import React from 'react';
+import { Button, Flex } from 'dt-design';
+
+const Demo: React.FC = () => (
+    <Flex wrap gap="small">
+        {Array.from({ length: 24 }, (_, i) => (
+            <Button key={i} type="primary">
+                Button
+            </Button>
+        ))}
+    </Flex>
+);
+
+export default Demo;
+`}}]);
---
+++ meta__progressBar.f94954d6.async.js
@@ -0,0 +1,26 @@
+"use strict";(self.webpackChunkdt_design=self.webpackChunkdt_design||[]).push([[9939],{37798:function(f,d,n){n.r(d),n.d(d,{demos:function(){return P}});var s=n(90228),m=n.n(s),u=n(87999),h=n.n(u),a=n(75271),P={"progressbar-demo-0":{component:a.memo(a.lazy(h()(m()().mark(function x(){var I,o,v,l,g,e;return m()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.resolve().then(n.t.bind(n,75271,19));case 2:return I=t.sent,o=I.default,t.next=6,Promise.all([n.e(8945),n.e(5136),n.e(5385),n.e(7732),n.e(9166),n.e(6082),n.e(6105),n.e(8704),n.e(2208),n.e(2276),n.e(4072),n.e(3479),n.e(3880),n.e(9520),n.e(4949),n.e(7029),n.e(9448),n.e(4886),n.e(2004),n.e(5954),n.e(7145),n.e(1130),n.e(5512),n.e(7430),n.e(9575),n.e(6117),n.e(9504),n.e(1911),n.e(27),n.e(8230),n.e(1966),n.e(4916),n.e(4654),n.e(9620),n.e(3697),n.e(1869),n.e(2784),n.e(8210)]).then(n.bind(n,64273));case 6:return v=t.sent,l=v.ProgressBar,t.next=10,Promise.all([n.e(8945),n.e(5385),n.e(6082),n.e(2208),n.e(4072),n.e(3880),n.e(7029),n.e(9448),n.e(2004),n.e(7145),n.e(1699),n.e(7316),n.e(2133),n.e(4464)]).then(n.bind(n,84464));case 10:return g=t.sent,e=g.Button,t.abrupt("return",{default:function(){return o.createElement(o.Fragment,null,o.createElement(e,{style:{margin:"40px 10px 0 20px"},type:"primary",onClick:function(){l.show()}},"\u53D1\u8D77\u8BF7\u6C42"),o.createElement(e,{onClick:function(){l.hide()}},"\u8BF7\u6C42\u7ED3\u675F"))}});case 13:case"end":return t.stop()}},x)})))),asset:{type:"BLOCK",id:"progressbar-demo-0",refAtomIds:["progressBar"],dependencies:{"index.jsx":{type:"FILE",value:`import React from 'react';
+import { ProgressBar } from 'dt-design';
+import { Button } from 'antd';
+
+export default () => {
+    return (
+        <>
+            <Button
+                style={{ margin: '40px 10px 0 20px' }}
+                type="primary"
+                onClick={() => {
+                    ProgressBar.show();
+                }}
+            >
+                \u53D1\u8D77\u8BF7\u6C42
+            </Button>
+            <Button
+                onClick={() => {
+                    ProgressBar.hide();
+                }}
+            >
+                \u8BF7\u6C42\u7ED3\u675F
+            </Button>
+        </>
+    );
+};`},react:{type:"NPM",value:"18.3.1"},"dt-design":{type:"NPM",value:"1.0.0"},antd:{type:"NPM",value:"4.24.16"}},entry:"index.jsx",title:"\u57FA\u7840\u4F7F\u7528"},context:void 0,renderOpts:void 0}}},88177:function(f,d,n){n.r(d),n.d(d,{texts:function(){return s}});const s=[{value:"\u7F51\u7EDC\u8BF7\u6C42\u8FC7\u7A0B\u63D0\u793A\u8FDB\u5EA6\u6761",paraId:0,tocIndex:1},{value:"\u53C2\u6570",paraId:1,tocIndex:4},{value:"\u8BF4\u660E",paraId:1,tocIndex:4},{value:"\u7C7B\u578B",paraId:1,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:4},{value:"show",paraId:1,tocIndex:4},{value:"\u663E\u793A\u8FDB\u5EA6\u6761",paraId:1,tocIndex:4},{value:"Function",paraId:1,tocIndex:4},{value:"-",paraId:1,tocIndex:4},{value:"hide",paraId:1,tocIndex:4},{value:"\u9690\u85CF\u8FDB\u5EA6\u6761",paraId:1,tocIndex:4},{value:"Function",paraId:1,tocIndex:4},{value:"-",paraId:1,tocIndex:4}]}}]);
