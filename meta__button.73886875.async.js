5828362500066fe3c245d9790abe8bbd489eb547
ZhaoFxxkSky <965752122@qq.com>
2025-12-16 20:08:38 +0800

    Updates

---
+++ meta__button.73886875.async.js
@@ -0,0 +1,208 @@
+"use strict";(self.webpackChunk_dtjoy_dt_design=self.webpackChunk_dtjoy_dt_design||[]).push([[2470],{11359:function(o,t,n){n.r(t),n.d(t,{demos:function(){return r}});var e=n(75271),r={"button-demo-basic":{component:e.memo(e.lazy(function(){return Promise.all([n.e(5591),n.e(3658),n.e(1708),n.e(3780),n.e(8704),n.e(4957),n.e(1821),n.e(4219)]).then(n.bind(n,66752))})),asset:{type:"BLOCK",id:"button-demo-basic",refAtomIds:["button"],dependencies:{"index.tsx":{type:"FILE",value:n(33814).Z},react:{type:"NPM",value:"18.3.1"},"@dtinsight/react-icons":{type:"NPM",value:"1.5.0"},"@dtjoy/dt-design":{type:"NPM",value:"1.0.2"},antd:{type:"NPM",value:"4.24.16"}},entry:"index.tsx",title:"\u57FA\u672C\u4F7F\u7528",description:"\u6309\u94AE\u7684\u57FA\u672C\u4F7F\u7528\uFF0C\u5305\u62EC\u4E0D\u540C\u7C7B\u578B\u7684\u6309\u94AE\u3001\u5E26\u56FE\u6807\u7684\u6309\u94AE\u3001\u7EAF\u56FE\u6807\u6309\u94AE\u3001\u4E0D\u540C\u5C3A\u5BF8\u7684\u6309\u94AE\u4EE5\u53CA\u7981\u7528\u72B6\u6001\u7684\u6309\u94AE\u3002"},context:void 0,renderOpts:void 0},"button-demo-loading":{component:e.memo(e.lazy(function(){return Promise.all([n.e(5591),n.e(3658),n.e(1708),n.e(3780),n.e(8704),n.e(4957),n.e(1821),n.e(4219)]).then(n.bind(n,95607))})),asset:{type:"BLOCK",id:"button-demo-loading",refAtomIds:["button"],dependencies:{"index.tsx":{type:"FILE",value:n(27288).Z},react:{type:"NPM",value:"18.3.1"},"@dtinsight/react-icons":{type:"NPM",value:"1.5.0"},antd:{type:"NPM",value:"4.24.16"},"@dtjoy/dt-design":{type:"NPM",value:"1.0.2"}},entry:"index.tsx",title:"\u52A0\u8F7D\u72B6\u6001",description:"\u5C55\u793A\u6309\u94AE\u7684\u52A0\u8F7D\u72B6\u6001\u548C\u5371\u9669\u6309\u94AE\u3002"},context:void 0,renderOpts:void 0},"button-demo-block":{component:e.memo(e.lazy(function(){return Promise.all([n.e(5591),n.e(3658),n.e(1708),n.e(3780),n.e(8704),n.e(4957),n.e(1821),n.e(4219)]).then(n.bind(n,83280))})),asset:{type:"BLOCK",id:"button-demo-block",refAtomIds:["button"],dependencies:{"index.tsx":{type:"FILE",value:n(5661).Z},react:{type:"NPM",value:"18.3.1"},"@dtinsight/react-icons":{type:"NPM",value:"1.5.0"},antd:{type:"NPM",value:"4.24.16"},"@dtjoy/dt-design":{type:"NPM",value:"1.0.2"}},entry:"index.tsx",title:"\u5757\u7EA7\u6309\u94AE",description:"\u5C55\u793A\u5757\u7EA7\u6309\u94AE\u548C\u5E7D\u7075\u6309\u94AE\u3002"},context:void 0,renderOpts:void 0}}},5359:function(o,t,n){n.r(t),n.d(t,{texts:function(){return e}});const e=[{value:"\u6309\u94AE\u7528\u4E8E\u5F00\u59CB\u4E00\u4E2A\u5373\u65F6\u64CD\u4F5C\u3002",paraId:0,tocIndex:1},{value:"Button \u7EC4\u4EF6\u652F\u6301 antd Button \u7EC4\u4EF6\u7684\u6240\u6709\u5C5E\u6027\uFF0C\u8BE6\u89C1 ",paraId:1,tocIndex:7},{value:"Ant Design Button API",paraId:1,tocIndex:7},{value:"\u3002",paraId:1,tocIndex:7},{value:"\u4F7F\u7528\u81EA\u5B9A\u4E49\u56FE\u6807\u65F6\uFF0C\u8BF7\u786E\u4FDD\u56FE\u6807\u7EC4\u4EF6\u7B26\u5408\u89C4\u8303\uFF0C\u5EFA\u8BAE\u4F7F\u7528\u9879\u76EE\u5185\u7684\u56FE\u6807\u7EC4\u4EF6\u3002",paraId:2,tocIndex:8},{value:"\u56FE\u6807\u5927\u5C0F\u4F1A\u6839\u636E\u6309\u94AE\u7684 ",paraId:2,tocIndex:8},{value:"size",paraId:2,tocIndex:8},{value:" \u5C5E\u6027\u81EA\u52A8\u8C03\u6574\uFF0C\u4E5F\u53EF\u4EE5\u901A\u8FC7\u8BBE\u7F6E\u56FE\u6807\u7EC4\u4EF6\u7684 ",paraId:2,tocIndex:8},{value:"style",paraId:2,tocIndex:8},{value:" \u5C5E\u6027\u6765\u624B\u52A8\u8C03\u6574\u5927\u5C0F\u3002",paraId:2,tocIndex:8},{value:"\u5F53\u53EA\u8BBE\u7F6E ",paraId:2,tocIndex:8},{value:"icon",paraId:2,tocIndex:8},{value:" \u800C\u4E0D\u8BBE\u7F6E ",paraId:2,tocIndex:8},{value:"children",paraId:2,tocIndex:8},{value:" \u65F6\uFF0C\u6309\u94AE\u5C06\u53EA\u663E\u793A\u56FE\u6807\u3002",paraId:2,tocIndex:8},{value:"\u672C\u7EC4\u4EF6\u662F\u5BF9 antd Button \u7684\u5C01\u88C5\uFF0C\u652F\u6301 antd Button \u7684\u6240\u6709\u5C5E\u6027\u548C\u4E8B\u4EF6\u3002",paraId:2,tocIndex:8}]},33814:function(o,t){t.Z=`import React, { useState } from 'react';\r
+import { UploadOutlined } from '@dtinsight/react-icons';\r
+import { Button } from '@dtjoy/dt-design';\r
+import { Radio, Space } from 'antd';\r
+\r
+import { ButtonProps } from '..';\r
+\r
+export default function Basic() {\r
+    const [size, setSize] = useState<ButtonProps['size']>('middle');\r
+\r
+    return (\r
+        <Space direction="vertical" size="large">\r
+            <Radio.Group value={size} onChange={(e) => setSize(e.target.value)}>\r
+                <Radio.Button value="large">Large</Radio.Button>\r
+                <Radio.Button value="middle">Default</Radio.Button>\r
+                <Radio.Button value="small">Small</Radio.Button>\r
+            </Radio.Group>\r
+\r
+            <div>\r
+                <h3>\u6309\u94AE\u7C7B\u578B</h3>\r
+                <Space>\r
+                    <Button type="primary" size={size}>\r
+                        Primary Button\r
+                    </Button>\r
+                    <Button type="secondary" size={size}>\r
+                        Secondary Button\r
+                    </Button>\r
+                    <Button type="tertiary" size={size}>\r
+                        Tertiary Button\r
+                    </Button>\r
+                    <Button size={size}>Default Button</Button>\r
+                    <Button type="dashed" size={size}>\r
+                        Dashed Button\r
+                    </Button>\r
+                    <Button type="link" size={size}>\r
+                        Link Button\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+\r
+            <div>\r
+                <h3>\u5E26\u56FE\u6807\u7684\u6309\u94AE</h3>\r
+                <Space>\r
+                    <Button type="primary" size={size} icon={<UploadOutlined />}>\r
+                        Search\r
+                    </Button>\r
+                    <Button icon={<UploadOutlined />} size={size}>\r
+                        Search\r
+                    </Button>\r
+                    <Button type="dashed" size={size} icon={<UploadOutlined />}>\r
+                        Search\r
+                    </Button>\r
+                    <Button type="link" size={size} icon={<UploadOutlined />}>\r
+                        Search\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+\r
+            <div>\r
+                <h3>\u7EAF\u56FE\u6807\u6309\u94AE</h3>\r
+                <Space>\r
+                    <Button type="primary" size={size} icon={<UploadOutlined />} />\r
+                    <Button size={size} icon={<UploadOutlined />} />\r
+                    <Button size={size} type="dashed" icon={<UploadOutlined />} />\r
+                    <Button size={size} type="link" icon={<UploadOutlined />} />\r
+                </Space>\r
+            </div>\r
+\r
+            <div>\r
+                <h3>\u7981\u7528\u72B6\u6001</h3>\r
+                <Space wrap>\r
+                    <Button size={size} type="primary" disabled>\r
+                        Primary(Disabled)\r
+                    </Button>\r
+                    <Button size={size} type="secondary" disabled>\r
+                        Secondary(Disabled)\r
+                    </Button>\r
+                    <Button size={size} type="tertiary" disabled>\r
+                        Tertiary(Disabled)\r
+                    </Button>\r
+                    <Button size={size} disabled>\r
+                        Default(Disabled)\r
+                    </Button>\r
+                    <Button size={size} type="dashed" disabled>\r
+                        Dashed(Disabled)\r
+                    </Button>\r
+                    <Button size={size} type="link" disabled>\r
+                        Link(Disabled)\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+        </Space>\r
+    );\r
+}\r
+`},5661:function(o,t){t.Z=`import React from 'react';\r
+import { UploadOutlined } from '@dtinsight/react-icons';\r
+import { Space } from 'antd';\r
+import { Button } from '@dtjoy/dt-design';\r
+\r
+export default function BlockDemo() {\r
+    return (\r
+        <Space direction="vertical" size="large" style={{ width: '100%' }}>\r
+            <div>\r
+                <h3>\u5757\u7EA7\u6309\u94AE</h3>\r
+                <Space direction="vertical" style={{ width: '100%' }}>\r
+                    <Button type="primary" block>\r
+                        Primary Block Button\r
+                    </Button>\r
+                    <Button block>Default Block Button</Button>\r
+                    <Button type="dashed" block>\r
+                        Dashed Block Button\r
+                    </Button>\r
+                    <Button type="link" block>\r
+                        Link Block Button\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+\r
+            <div style={{ background: 'rgb(190, 200, 200)', padding: '16px' }}>\r
+                <h3>\u5E7D\u7075\u6309\u94AE</h3>\r
+                <Space>\r
+                    <Button type="primary" ghost>\r
+                        Primary Ghost\r
+                    </Button>\r
+                    <Button ghost>Default Ghost</Button>\r
+                    <Button type="dashed" ghost>\r
+                        Dashed Ghost\r
+                    </Button>\r
+                    <Button type="primary" ghost icon={<UploadOutlined />}>\r
+                        Ghost with Icon\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+        </Space>\r
+    );\r
+}\r
+`},27288:function(o,t){t.Z=`import React, { useState } from 'react';\r
+import { UploadOutlined } from '@dtinsight/react-icons';\r
+import { Space } from 'antd';\r
+import { Button } from '@dtjoy/dt-design';\r
+\r
+export default function LoadingDemo() {\r
+    const [loading, setLoading] = useState(false);\r
+\r
+    const handleClick = () => {\r
+        setLoading(true);\r
+        setTimeout(() => {\r
+            setLoading(false);\r
+        }, 2000);\r
+    };\r
+\r
+    return (\r
+        <Space direction="vertical" size="large">\r
+            <div>\r
+                <h3>\u52A0\u8F7D\u72B6\u6001</h3>\r
+                <Space>\r
+                    <Button type="primary" loading>\r
+                        Loading\r
+                    </Button>\r
+                    <Button loading>Loading</Button>\r
+                    <Button type="dashed" loading>\r
+                        Loading\r
+                    </Button>\r
+                    <Button type="link" loading>\r
+                        Loading\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+\r
+            <div>\r
+                <h3>\u70B9\u51FB\u540E\u52A0\u8F7D</h3>\r
+                <Space>\r
+                    <Button type="primary" loading={loading} onClick={handleClick}>\r
+                        Click me!\r
+                    </Button>\r
+                    <Button loading={loading} onClick={handleClick}>\r
+                        Click me!\r
+                    </Button>\r
+                    <Button\r
+                        type="primary"\r
+                        icon={<UploadOutlined />}\r
+                        loading={loading}\r
+                        onClick={handleClick}\r
+                    >\r
+                        Click me!\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+\r
+            <div>\r
+                <h3>\u5371\u9669\u6309\u94AE</h3>\r
+                <Space>\r
+                    <Button type="primary" danger>\r
+                        Primary Danger\r
+                    </Button>\r
+                    <Button danger>Default Danger</Button>\r
+                    <Button type="dashed" danger>\r
+                        Dashed Danger\r
+                    </Button>\r
+                    <Button type="link" danger>\r
+                        Link Danger\r
+                    </Button>\r
+                </Space>\r
+            </div>\r
+        </Space>\r
+    );\r
+}\r
+`}}]);
