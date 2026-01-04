532cb2f81b2a1f71d99d410303b614386c5646b5
xiyan <wx-zhaog@dtjoy.com>
2026-01-04 17:11:06 +0800

    fix(fatherrc): add missing input property to esm and cjs configurations

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'esm', ignores: ['**/demos/**'], transformer: 'babel' },
-  cjs: { output: 'lib', ignores: ['**/demos/**'], transformer: 'babel' },
+  esm: { input: 'components', output: 'esm', ignores: ['**/demos/**'], transformer: 'babel' },
+  cjs: { input: 'components', output: 'lib', ignores: ['**/demos/**'], transformer: 'babel' },
 });
