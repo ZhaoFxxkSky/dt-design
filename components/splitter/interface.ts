d8dc5a05c9ebb762744deed00d9e3384b3ab724b
ritian <wx-zhaog@dtjoy.com>
2026-07-04 14:58:56 +0800

    chore(splitter): remove unused interfaces and add id to InternalPanelProps

--- components/splitter/interface.ts
+++ components/splitter/interface.ts
@@ -29,45 +29,5 @@ export interface PanelProps {
 export interface InternalPanelProps extends PanelProps {
   className?: string;
   prefixCls?: string;
-}
-
-export interface UseResizeProps extends Pick<SplitterProps, 'onResize'> {
-  basicsState: number[];
-  items: PanelProps[];
-  panelsRef: React.RefObject<(HTMLDivElement | null)[]>;
-  reverse: boolean;
-  setBasicsState: React.Dispatch<React.SetStateAction<number[]>>;
-}
-
-export interface UseResize {
-  setSize: (data: { size: number; index: number }[]) => void;
-  setOffset: (offset: number, containerSize: number, index: number) => void;
-}
-
-export interface UseHandleProps
-  extends Pick<SplitterProps, 'layout' | 'onResizeStart' | 'onResizeEnd'> {
-  basicsState: number[];
-  containerRef?: React.RefObject<HTMLDivElement | null>;
-  setOffset: UseResize['setOffset'];
-  setResizing: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export interface UseHandle {
-  onStart: (x: number, y: number, index: number) => void;
-}
-
-export interface UseCollapsibleProps {
-  basicsState: number[];
-  collapsible?: PanelProps['collapsible'];
-  index: number;
-  reverse: boolean;
-  setSize?: UseResize['setSize'];
-}
-
-export interface UseCollapsible {
-  nextIcon: boolean;
-  overlap: boolean;
-  previousIcon: boolean;
-  onFold: (type: 'previous' | 'next') => void;
-  setOldBasics: () => void;
+  id?: string;
 }
