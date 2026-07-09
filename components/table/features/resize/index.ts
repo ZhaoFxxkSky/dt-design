922771d66e53b406fd389a36ef88f081ac243226
xiyan <wx-zhaog@dtjoy.com>
2026-07-09 10:09:55 +0800

    fix(table): remove missing transformResizableColumns export

--- components/table/features/resize/index.ts
+++ components/table/features/resize/index.ts
@@ -1,5 +1,3 @@
 export { default as ResizeHandle } from './ResizeHandle';
-export { default as transformResizableColumns } from './transformResizableColumns';
-export type { TransformResizableOptions } from './transformResizableColumns';
 export { default as useResize } from './useResize';
 export type { UseResizeOptions } from './useResize';
